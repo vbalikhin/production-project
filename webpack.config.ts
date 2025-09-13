581bd5665c3eddb3e26e48c2d2586f441275b6a6
Vitaly Balikhin <balikhinv@bk.ru>
2025-09-13 19:41:44 +0300

    init commit

---
+++ webpack.config.ts
@@ -0,0 +1,23 @@
+import webpack from "webpack";
+import {buildWebpackConfig} from "./config/buildConfig/buildWebpackConfig";
+import {BuildPaths} from "./config/buildConfig/types/config";
+import path from "path";
+
+const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+}
+
+const mode = 'development'
+const isDev = mode === 'development'
+const PORT = 3000
+
+const config: webpack.Configuration = buildWebpackConfig({
+    mode: 'development',
+    paths,
+    isDev,
+    port: PORT
+})
+
+export default config
