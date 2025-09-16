d8a8ddf8847cfa5c8d370b613122909d58859fe6
Vitaly Balikhin <balikhinv@bk.ru>
2025-09-16 22:47:26 +0300

    Theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React, {Suspense} from 'react';
+import React, {Suspense, useContext, useState} from 'react';
 import {Link, Route, Routes} from 'react-router-dom'
-import './index.scss'
 import {MainLazy} from "./pages/Main/Main.lazy";
 import {InfoLazy} from "./pages/Info/Info.lazy";
+import './styles/index.scss'
+import {useTheme} from "./theme/useTheme";
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
     return (
-        <div className={'app'}>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/info'}>Инфо</Link>
             <Suspense fallback={<div>Loading...</div>}>
