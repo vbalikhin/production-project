ffd4cbd284be481619c71323b6e86bd1a83462a0
Vitaly Balikhin <balikhinv@bk.ru>
2025-09-17 11:08:52 +0300

    classNames helper

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React, {Suspense, useContext, useState} from 'react';
+import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom'
 import {MainLazy} from "./pages/Main/Main.lazy";
 import {InfoLazy} from "./pages/Info/Info.lazy";
 import './styles/index.scss'
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames(`app`, {}, [theme])}>
             <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/info'}>Инфо</Link>
