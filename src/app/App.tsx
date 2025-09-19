a29f775e5f3b51d44501cfce653c98cbf2694989
Vitaly Balikhin <balikhinv@bk.ru>
2025-09-19 21:50:22 +0300

    sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,13 +4,17 @@ import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/router";
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 const App = () => {
     const {theme} = useTheme()
     return (
         <div className={classNames(`app`, {}, [theme])}>
             <Navbar/>
-            <AppRouter/>
+            <div className={'content'}>
+                <Sidebar/>
+                <AppRouter/>
+            </div>
         </div>
     );
 };
---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,21 @@
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './Sidebar.module.scss'
+import {useState} from "react";
+
+interface SidebarProps {
+    className?: string
+}
+
+export const Sidebar = ({className}: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false)
+    const onToggle = () => {
+        setCollapsed(prev => !prev)
+    }
+    return (
+        <div className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}>
+            <button onClick={onToggle}>toggle</button>
+        </div>
+    );
+};
+
+
